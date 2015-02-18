fe9000abec6132da28b59101b75d57e15bd23076
MHO <mathieu.hopmann@smartliberty.ch>
2015-02-18 17:19:59 +0100

    Second develop commit

--- app/src/main/java/smartliberty/gitflowtest/MainActivity.java
+++ app/src/main/java/smartliberty/gitflowtest/MainActivity.java
@@ -61,6 +61,7 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            // Second develop commit
             return rootView;
         }
     }

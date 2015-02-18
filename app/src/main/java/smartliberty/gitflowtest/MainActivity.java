0c29a0a5592ba18b72ec599ef5070c90db2d68bc
MHO <mathieu.hopmann@smartliberty.ch>
2015-02-18 17:21:33 +0100

    Third develop commit

--- app/src/main/java/smartliberty/gitflowtest/MainActivity.java
+++ app/src/main/java/smartliberty/gitflowtest/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         // First develop commit
+        // Third develop commit
         return true;
     }
 
